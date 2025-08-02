9e63da159ede58b39fb0392989b84a8b438f33cb
Prince <princedarji716@gmail.com>
2025-08-02 13:04:02 +0530

    Changes for deployment

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from dotenv import load_dotenv
-
-load_dotenv()
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
